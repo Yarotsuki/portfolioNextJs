153b3300e124e1d32e361884d1eb59e2ecb98c8f
Yarotsuki <corbel.victorien@gmail.com>
2025-03-19 15:12:31 +0100

    changement boutton

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -32,7 +32,7 @@ export const Sidebar = () => {
               <Navigation setOpen={setOpen} />
             </div>
             <div onClick={() => isMobile() && setOpen(false)}>
-              <Badge href="/resume" text="Read Resume" />
+              <Badge href="/resume" text="Mes expériences" />
             </div>
           </motion.div>
         )}
